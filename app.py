cee95c4f98e6bc6293f5947cee661fb9c2836f40
IvanTsvetkov <tsvetkovivan365@gmail.com>
2025-11-04 15:21:04 +0200

    Configuring database URI

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from database import db
+from os import path
 
 # Flask App Initialization
 app = Flask(__name__)
 
+basedir = path.abspath(path.dirname(__file__))
+
 # Configuring database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + path.join(basedir, 'flask_app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 #Initializing the database extension with the application
 db.init_app(app)
