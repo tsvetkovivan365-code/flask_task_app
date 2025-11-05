78bf6280483f94b1c33cfc1c9fcaab76848401dd
IvanTsvetkov <tsvetkovivan365@gmail.com>
2025-11-05 17:13:01 +0200

    Setting up Flask Security

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from database import db
+from flask import Flask, render_template_string
+from flask_security import Security, SQLAlchemySessionUserDatastore, current_user, auth_required, hash_password
+from database import db, User, Task
 from os import path
 
 # Flask App Initialization
@@ -14,9 +15,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #Initializing the database extension with the application
 db.init_app(app)
 
+# Setup Flask-Security
+user_datastore = SQLAlchemySessionUserDatastore(app, User, Task)
+security = Security(app, user_datastore)
+
 @app.route('/')
-def index():
-    return 'Flask is working'
+@auth_required()
+def home():
+    return render_template_string(f"Hello, {{current_user.email}}")
+
 
 # Run application
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,14 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_security.models import sqla
 
 # Initializing SQLAlchemy object
 db = SQLAlchemy()
 
+# Setting up roles_users association table 
+roles_users = db.Table(
+    'roles_users',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+    db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+)
+
 # Defining the User model/table
-class User(db.Model):
+class User(db.Model, sqla.FsUserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), unique=True, nullable=False)
     email = db.Column(db.String(25), unique=True, nullable=False)
+
+    # Task relationship
     tasks = db.relationship('Task', backref='owner', lazy='dynamic')
+
+    # Role relationship via association table
+    role = db.relationship('Role', secondary=roles_users, backref='users', lazy='dynamic')
 
     def __repr__(self):
         return f'User {self.name}'
@@ -24,3 +37,9 @@ class Task(db.Model):
 
     def __repr__(self):
         return f'<Task {self.id}: {self.title}, User ID: {self.user_id}>'
+    
+# Defining the Role model/table
+class Role(db.Model, sqla.FsRoleMixin): 
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40), unique=True)
+    description = db.Column(db.Text)
