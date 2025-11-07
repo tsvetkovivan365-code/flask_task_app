d781bcb28a7d37da1a860a639c2af87fcad17314
IvanTsvetkov <tsvetkovivan365@gmail.com>
2025-11-07 15:16:21 +0200

    Installing flask-tailwindcss library

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').config} */
+module.exports = {
+    mode: "jit",
+    content: [
+        "./templates/*"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
